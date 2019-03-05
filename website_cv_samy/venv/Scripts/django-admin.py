40892ebc16eddc8df5a49c75edfb3ae9ed361282
Samy <samybencharef@gmail.com>
2019-03-05 18:17:04 +0100

    Creation of the website

---
+++ website_cv_samy/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\samyd\Desktop\TOUT\Git_Samy\website-django\website_cv_samy\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ website_cv_samy/venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\samyd\Desktop\TOUT\Git_Samy\website-django\website_cv_samy\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3'
+__requires__ = 'pip==10.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip3')()
+    )
